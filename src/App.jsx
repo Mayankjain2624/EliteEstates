f856db970e2b618dd8ee759b031a7e520adb4a31
Mayankjain2624 <mayankjain2624@gmail.com>
2024-12-28 01:07:05 +0530

    create routes and paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import { BroweserRoutrer,Route,Routes,route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import About from './pages/About'
+import Profie from './pages/Profie'
 function App() {
   return (
-    <div className='text-red-500'>
-      hello4
-    </div>
+    <BroweserRoutrer>
+    <Routes>
+      <Route path="/" element={Home}/>
+      <Route path="/signin" element={Signin}/>
+      <Route path="/signup" element={Signup}/>
+      <Route path="/about" element={About}/>
+      <Route path="/profile" element={Profie}/>
+      </Routes>
+    </BroweserRoutrer>
   )
 }
 
